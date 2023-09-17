b7807c99e8d496ebc357e7a81918f94f2a01d060
juan david vega <juan.vega01@uceva.edu.co>
2023-09-17 13:56:08 -0500

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include  <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+const char * ssid = "JUANDAVID";
+const char * pass = "123456789";
+const char *host = "dweet.io";
+const int port = 80;
+
+/**
+* @brief Funcion que inicializa el dispositivo
+* Puerto serial(velocidad)
+* Direcciones de pines de entrada y salida
+* Se pueden crear objetos que representan sensores, dispositivos wifi y objetos de librerias externas
+*/
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); //configuramos a 115200 bits por segundo
+  WiFi.begin(ssid, pass); //Inicializamos el modulo Wifi y que se vincule a la red indicada en el ssid
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println(" Conectado!");
+  Serial.print("Direccion IP: ");
+  Serial.println(WiFi.localIP());
+
+  pinMode(2, OUTPUT); //Configuramos el pin 2 como salida
+  digitalWrite(2, HIGH); //Encendemos el LED 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  WiFiClient cliente; //Objeto que realiza peticiones a un socket servidor
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(500); //Retardo de 500ms
+  digitalWrite(2, LOW); //Apagar el Led
+  delay(500);
+  digitalWrite(2, HIGH); //Encender el LED
 }
